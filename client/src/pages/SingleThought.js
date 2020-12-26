3c44591f7e3fbaf976eb5ac08cfa901d9f0765eb
Mahmoud Abdulrhman <mahmoudtheeb@gmail.com>
2020-12-26 00:08:49 -0800

    add reaction

--- client/src/pages/SingleThought.js
+++ client/src/pages/SingleThought.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+
 import ReactionList from '../components/ReactionList';
+import ReactionForm from '../components/ReactionForm';
 
-
-
-
+import Auth from '../utils/auth';
 import { useQuery } from '@apollo/react-hooks';
 import { QUERY_THOUGHT } from '../utils/queries';
 
@@ -23,20 +23,22 @@ const SingleThought = props => {
 
   return (
     <div>
-  <div className="card mb-3">
-    <p className="card-header">
-      <span style={{ fontWeight: 700 }} className="text-light">
-        {thought.username}
-      </span>{' '}
-      thought on {thought.createdAt}
-    </p>
-    <div className="card-body">
-      <p>{thought.thoughtText}</p>
-    </div>
-  </div>
-  {thought.reactionCount > 0 && <ReactionList reactions={thought.reactions} />}
-</div>
+      <div className="card mb-3">
+        <p className="card-header">
+          <span style={{ fontWeight: 700 }} className="text-light">
+            {thought.username}
+          </span>{' '}
+          thought on {thought.createdAt}
+        </p>
+        <div className="card-body">
+          <p>{thought.thoughtText}</p>
+        </div>
+      </div>
 
+      {thought.reactionCount > 0 && <ReactionList reactions={thought.reactions} />}
+
+      {Auth.loggedIn() && <ReactionForm thoughtId={thought._id} />}
+    </div>
   );
 };
 
